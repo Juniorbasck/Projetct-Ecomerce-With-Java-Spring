5cc34bc320cde7cfff82885c60e6c8618ad75818
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2023-11-14 17:50:09 -0300

    add method of deleting

--- backend/src/main/java/com/desafio/backend/cart/Controller/CartController.java
+++ backend/src/main/java/com/desafio/backend/cart/Controller/CartController.java
@@ -4,10 +4,7 @@ import com.desafio.backend.cart.Cart;
 import com.desafio.backend.cart.Service.CartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.List;
@@ -22,5 +19,17 @@ public class CartController {
     @GetMapping("/{cartId}")
     public Cart getTheCart(@PathVariable Long cartId){
         return cartRepository.findCartById(cartId);
+    }
+
+    @DeleteMapping("/{cartId/{productId}")
+    public  Cart deleteProduct(@PathVariable Long cartId, @PathVariable Long productId) {
+        Cart cart = cartRepository.findById(cartId).orElse(null);
+
+        if (cart != null) {
+            cart.getItem().removeIf(item -> item.getProduct().getId().equals(productId));
+            return cartRepository.save(cart);
+        } else {
+            return null;
+        }
     }
 }
