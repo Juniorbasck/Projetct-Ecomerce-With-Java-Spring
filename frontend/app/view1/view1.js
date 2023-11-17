accc7137ccebc777051110f89a55abfad869364e
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2023-11-17 03:53:18 -0300

    home page

--- frontend/app/view1/view1.js
+++ frontend/app/view1/view1.js
@@ -9,6 +9,55 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
 
+  $http.get('http://localhost:8080/products')
+    .then(function(response) {
+      $scope.produtos = response.data;
+    })
+    .catch(function(error) {
+      console.error('Erro ao obter a lista de produtos', error);
+    });
+
+
+
+    $scope.adicionarNovoProduto = function() {
+
+      var precoNumero = parseFloat($scope.novoProduto.price);
+  
+      var novoProduto = {
+          name: $scope.novoProduto.name,
+          price: precoNumero
+      };
+  
+      $http.post('http://localhost:8080/products', novoProduto)
+          .then(function(response) {
+              console.log('Novo produto adicionado com sucesso:', response.data);
+  
+              $scope.novoProduto = {};
+              $scope.mensagemSucesso = 'Novo produto adicionado com sucesso!';
+  
+              return $http.get('http://localhost:8080/products');
+          })
+          .then(function(response) {
+              $scope.produtos = response.data;
+          })
+          .catch(function(error) {
+              console.error('Erro ao adicionar novo produto', error);
+          });
+    };
+
+    $scope.excluirProduto = function(id) {
+      $http.delete('http://localhost:8080/products/' + id)
+          .then(function(response) {
+              console.log('Produto excluído com sucesso:', response.data);
+  
+              $scope.produtos = $scope.produtos.filter(function(produto) {
+                  return produto.id !== id;
+              });
+          })
+          .catch(function(error) {
+              console.error('Erro ao excluir produto', error);
+          });
+     };
 }]);
