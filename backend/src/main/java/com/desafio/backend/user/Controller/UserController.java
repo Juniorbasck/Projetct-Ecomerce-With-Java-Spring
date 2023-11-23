e53380c7d3e916d5d6b91df313aab33b779924dc
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2023-11-23 17:47:34 -0300

    add controller

--- backend/src/main/java/com/desafio/backend/user/Controller/UserController.java
+++ backend/src/main/java/com/desafio/backend/user/Controller/UserController.java
@@ -1,5 +1,20 @@
 package com.desafio.backend.user.Controller;
 
+import com.desafio.backend.user.Services.UserReposity;
+import com.desafio.backend.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private UserReposity userReposity;
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public User CreateUse(@RequestBody User user){
+        return userReposity.save(user);
+    }
 }
--- backend/src/main/java/com/desafio/backend/user/Services/UserReposity.java
+++ backend/src/main/java/com/desafio/backend/user/Services/UserReposity.java
@@ -1,9 +1,10 @@
 package com.desafio.backend.user.Services;
 
+import com.desafio.backend.user.User;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Transactional
-public interface UserReposity extends JpaRepository {
+public interface UserReposity extends JpaRepository<User, Long> {
 
 }
